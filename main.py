40c2beade348333d08170478e79c3ec883b1feef
CarlosOrtiz19 <carlos.arturo.ortiz@hotmail.com>
2025-02-10 21:09:12 -0500

    script alerts

---
+++ main.py
@@ -0,0 +1,8 @@
+
+from service.Trading import monitor_volume
+from service.messageTelegram import send_telegram_message
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    monitor_volume()
+
