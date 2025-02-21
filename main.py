d738b14cb2d5622e64090d34e7df7eec5a3847ef
CarlosOrtiz19 <carlos.arturo.ortiz@hotmail.com>
2025-02-20 20:21:21 -0500

    set config binance

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import logging
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    logging.info("Testing send_telegram_message function")
+
     monitor_volume()
 
--- src/service/messageTelegram.py
+++ src/service/messageTelegram.py
@@ -1,12 +1,18 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-TOKEN = "change_me"
-CHAT_ID = "change_me"
+load_dotenv()
+
+# Recuperar las variables de entorno
+TOKEN = os.environ.get("TELEGRAM_TOKEN", "change_me")
+CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "change_me")
 
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": message}
     response = requests.post(url, json=payload)
+    print(response.ok)
 
     return response.json()
